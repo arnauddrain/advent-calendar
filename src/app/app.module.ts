8f432774bf26a8ac9e0e22aaa44cee0d3b2e56c1
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-11-22 23:56:33 +0100

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAsBtxr5-Yin0s-3wfhyStn4VESdGjQxOA',
+    authDomain: 'advent-calendar-b80d7.firebaseapp.com',
+    databaseURL: 'https://advent-calendar-b80d7.firebaseio.com',
+    projectId: 'advent-calendar-b80d7',
+    storageBucket: 'advent-calendar-b80d7.appspot.com',
+    messagingSenderId: '463287392377',
+    appId: '1:463287392377:web:d265b8498dd54218c25ca8',
+    measurementId: 'G-RNKWRY2S12'
+
+  }
 };
 
 /*
