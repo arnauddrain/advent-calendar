aea1b265fcb50d60a54b41483bd94ef5a52ed2cd
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-11-29 00:33:58 +0100

    Enable iFrames

--- src/app/day-dialog/day-dialog.component.ts
+++ src/app/day-dialog/day-dialog.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-day-dialog',
@@ -7,12 +9,13 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./day-dialog.component.css']
 })
 export class DayDialogComponent {
-  text: string;
+  text: SafeHtml;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: { text: string }
+    @Inject(MAT_DIALOG_DATA) public data: { text: string },
+    private sanitizer: DomSanitizer
   ) {
-    this.text = data.text;
+    this.text = this.sanitizer.bypassSecurityTrustHtml(data.text ?? '<i>Le père noël n\'est pas encore passé par là...');
   }
 
 }
