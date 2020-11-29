3c0fac686a141f7f02697b401faa56122984883f
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-11-29 22:26:38 +0100

    Add dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAsBtxr5-Yin0s-3wfhyStn4VESdGjQxOA',
-    authDomain: 'advent-calendar-b80d7.firebaseapp.com',
-    databaseURL: 'https://advent-calendar-b80d7.firebaseio.com',
-    projectId: 'advent-calendar-b80d7',
-    storageBucket: 'advent-calendar-b80d7.appspot.com',
-    messagingSenderId: '463287392377',
-    appId: '1:463287392377:web:d265b8498dd54218c25ca8',
-    measurementId: 'G-RNKWRY2S12'
+    apiKey: 'AIzaSyCPH5aQv8cD5z80VVmq5NOTBDKrUiUbnss',
+    authDomain: 'advent-calendar-dev.firebaseapp.com',
+    databaseURL: 'https://advent-calendar-dev.firebaseio.com',
+    projectId: 'advent-calendar-dev',
+    storageBucket: 'advent-calendar-dev.appspot.com',
+    messagingSenderId: '317779781951',
+    appId: '1:317779781951:web:2c4f4c0e5b9ea8ebc34b8a'
   }
 };
 
