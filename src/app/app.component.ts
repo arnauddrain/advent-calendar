8750f481e7e08f56ec14229dacbd4f8bcfff8bce
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-12-18 08:59:02 +0100

    Donation only when logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFireAnalytics } from '@angular/fire/analytics';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user: firebase.User | null = null;
+
   constructor(
     public router: Router,
-    private analytics: AngularFireAnalytics
-  ) { }
+    private analytics: AngularFireAnalytics,
+    public auth: AngularFireAuth
+  ) {
+    auth.user.subscribe((user) => {
+      this.user = user;
+    });
+  }
 
   event() {
     this.analytics.logEvent('Click on donation');
