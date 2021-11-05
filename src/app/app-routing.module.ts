1ec1b12e18b5a882f493f7d34022a725645516c1
Arnaud Drain <drain.arnaud@hotmail.fr>
2021-11-05 17:27:22 +0100

    handle route authorization locally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AngularFireAuthGuard, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 
 import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarsComponent } from './calendars/calendars.component';
@@ -8,22 +7,15 @@ import { EditCalendarComponent } from './edit-calendar/edit-calendar.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-const redirectUnauthorizedToHome = () => redirectUnauthorizedTo(['']);
-const redirectLoggedInToCalendars = () => redirectLoggedInTo(['calendars']);
-
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectLoggedInToCalendars } },
+  { path: '', component: HomeComponent },
   {
     path: 'calendars',
-    component: CalendarsComponent,
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectUnauthorizedToHome }
+    component: CalendarsComponent
   },
   {
     path: 'edit/:uid',
-    component: EditCalendarComponent,
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectUnauthorizedToHome }
+    component: EditCalendarComponent
   },
   { path: ':uid', component: CalendarComponent },
   { path: '**', component: NotFoundComponent }
--- src/app/calendars/calendars.component.ts
+++ src/app/calendars/calendars.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { AngularFireAnalytics } from '@angular/fire/compat/analytics';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Router } from '@angular/router';
 import firebase from 'firebase/compat/app';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-calendars',
@@ -14,24 +15,33 @@ import { map } from 'rxjs/operators';
 })
 export class CalendarsComponent {
   newCalendarName: string = '';
-  calendars: Observable<any[]> | null;
+  calendars: Observable<any[]> | null = null;
   user: firebase.User | null = null;
   addingCalendar = false;
+  loading = true;
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFireDatabase,
     private analytics: AngularFireAnalytics,
-    private router: Router
+    private router: Router,
+    @Inject(PLATFORM_ID) platformId: string
   ) {
-    this.calendars = null;
-    auth.user.subscribe((user) => {
-      this.user = user;
-      this.calendars = this.db
-        .list('/calendars', (ref) => ref.orderByChild('author').equalTo(this.user?.uid ?? ''))
-        .snapshotChanges()
-        .pipe(map((changes) => changes.map((c) => ({ key: c.payload.key, value: c.payload.val() }))));
-    });
+    if (isPlatformBrowser(platformId)) {
+      auth.user.subscribe((user) => {
+        if (!user) {
+          this.router.navigate(['/']);
+        }
+        this.user = user;
+        this.calendars = this.db
+          .list('/calendars', (ref) => ref.orderByChild('author').equalTo(this.user?.uid ?? ''))
+          .snapshotChanges()
+          .pipe(
+            tap(() => (this.loading = false)),
+            map((changes) => changes.map((c) => ({ key: c.payload.key, value: c.payload.val() })))
+          );
+      });
+    }
   }
 
   async addCalendar() {
