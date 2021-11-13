2d6b753277b4087b51805def3f3b1dd403d8f2de
Arnaud Drain <drain.arnaud@hotmail.fr>
2021-11-13 15:40:46 +0100

    handle login errors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Analytics, logEvent } from '@angular/fire/analytics';
 import { Auth, user, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from '@angular/fire/auth';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { Router } from '@angular/router';
+import { signInWithRedirect } from '@firebase/auth';
 
 import { BottomSheetComponent } from '../bottom-sheet/bottom-sheet.component';
 
@@ -19,11 +20,31 @@ export class HomeComponent {
     });
   }
 
+  async loginViaRedirect(provider: typeof GoogleAuthProvider | typeof FacebookAuthProvider) {
+    await signInWithRedirect(this.auth, new provider());
+    this.router.navigate(['calendars']);
+  }
+
+  private checkForCommonError(e: any, provider: typeof GoogleAuthProvider | typeof FacebookAuthProvider) {
+    if (e.code === 'auth/popup-blocked') {
+      this.loginViaRedirect(provider);
+      return true;
+    }
+    return ['auth/popup-closed-by-user', 'auth/cancelled-popup-request'].includes(e.code);
+  }
+
   async login() {
     logEvent(this.analytics, 'Login');
     logEvent(this.analytics, 'Login via Google');
-    await signInWithPopup(this.auth, new GoogleAuthProvider());
-    this.router.navigate(['calendars']);
+    try {
+      await signInWithPopup(this.auth, new GoogleAuthProvider());
+      this.router.navigate(['calendars']);
+    } catch (e: any) {
+      if (this.checkForCommonError(e, GoogleAuthProvider)) {
+        return;
+      }
+      throw e;
+    }
   }
 
   async fbLogin() {
@@ -31,7 +52,11 @@ export class HomeComponent {
     logEvent(this.analytics, 'Login via Facebook');
     try {
       await signInWithPopup(this.auth, new FacebookAuthProvider());
+      this.router.navigate(['calendars']);
     } catch (e: any) {
+      if (this.checkForCommonError(e, FacebookAuthProvider)) {
+        return;
+      }
       if (e.code === 'auth/account-exists-with-different-credential') {
         logEvent(this.analytics, 'Error with login via Facebook');
         this.bottomSheet.open(BottomSheetComponent, {
@@ -39,11 +64,9 @@ export class HomeComponent {
             text: 'Cet email est déjà associé à un autre compte, essayez de vous connecter avec Google'
           }
         });
-      } else {
-        throw e;
+        return;
       }
-      return;
+      throw e;
     }
-    this.router.navigate(['calendars']);
   }
 }
