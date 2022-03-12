6b38259f3e49540515dff1a5581da553c1b66756
Arnaud Drain <drain.arnaud@hotmail.fr>
2022-03-12 19:55:09 +0100

    remove every reference to tests

--- src/app/shared/bypass.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BypassPipe } from './bypass.pipe';
-
-describe('BypassPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BypassPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
