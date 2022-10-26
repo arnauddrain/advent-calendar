2fdc14dcde89638bf9dc5b55b86f471a5af4cd16
Arnaud Drain <drain.arnaud@hotmail.fr>
2022-10-26 10:09:15 +0200

    add / route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const redirectUnauthorizedToHome = () => redirectUnauthorizedTo(['']);
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: '/', component: HomeComponent },
   {
     path: 'calendars',
     component: CalendarsComponent,
