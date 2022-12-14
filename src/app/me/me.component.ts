5a75ab468f0b3ee5e2e5794e540dc0a3ddddc8a5
Arnaud Drain <drain.arnaud@hotmail.fr>
2022-12-14 12:07:33 +0100

    show uid in /me

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -3,7 +3,7 @@ import { Auth, User, user } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-me',
-  template: '<p>Bonjour</p><p *ngIf="user">Email : {{ user.email }}</p><p *ngIf="user">Nom complet : {{ user.displayName }}</p>'
+  template: '<p>Bonjour</p><p *ngIf="user">Email : {{ user.email }}</p><p *ngIf="user">Nom complet : {{ user.displayName }}</p><p *ngIf="user">Uid : {{ user.uid }}</p>'
 })
 export class MeComponent {
   user: User | null = null;
